76b318103a7992d0146723a04ff16fdd6b40d797
DennisEckerskorn <denniseckerskorn@gmail.com>
2024-09-21 19:01:10 +0200

    Updated comments in MainActivity

--- app/src/main/java/com/denniseckerskorn/tema03ejercicio02/MainActivity.java
+++ app/src/main/java/com/denniseckerskorn/tema03ejercicio02/MainActivity.java
@@ -19,16 +19,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Se establece la actividad:
         setContentView(R.layout.activity_main);
 
+        //Enlaces de los elementos de la interfaz de usuario:
         EditText etNumber = findViewById(R.id.etNumber);
         Button bCalculate = findViewById(R.id.bCalculate);
         TextView tvResult = findViewById(R.id.tvResult);
 
+        //Comportamiento del boton al hacer click:
         bCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Llamada al método estático de otra clase (MathUtils):
                 long result = MathUtils.factorial(Integer.parseInt(String.valueOf(etNumber.getText())));
+                //Muestra el resultado del calculo:
                 tvResult.setText(String.valueOf(result));
             }
         });
